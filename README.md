fbf5c07d4d058370347c94015046412803abbfb7
Lei Zhang <coolbeevip@gmail.com>
2015-02-04 14:20:58 +0800

    基于VJDBC，增加了可定义server rmi address 和 client rmi port

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 虚拟JDBC，可作JDBC代理服务
+
+基于VJDBC，增加了可定义server rmi address 和 client rmi port
+
+当服务器上存在多ip时，可以使用 -Djava.rmi.server.hostname=X.X.X.X 的方式绑定服务器ip
+存在防火墙时可以使用 -Djava.rmi.client.port=XX 的方式指定客户端连接端口
--- src/de/simplicit/vjdbc/server/rmi/ConnectionServer.java
+++ src/de/simplicit/vjdbc/server/rmi/ConnectionServer.java
@@ -4,99 +4,122 @@
 
 package de.simplicit.vjdbc.server.rmi;
 
-import de.simplicit.vjdbc.rmi.SecureSocketFactory;
-import de.simplicit.vjdbc.server.config.RmiConfiguration;
-import de.simplicit.vjdbc.server.config.VJdbcConfiguration;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.RMISocketFactory;
 import java.util.Properties;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.simplicit.vjdbc.rmi.SecureSocketFactory;
+import de.simplicit.vjdbc.server.config.RmiConfiguration;
+import de.simplicit.vjdbc.server.config.VJdbcConfiguration;
+
 public class ConnectionServer {
-    private static Log _logger = LogFactory.getLog(ConnectionServer.class);
+	private static Log _logger = LogFactory.getLog(ConnectionServer.class);
+	private RmiConfiguration _rmiConfiguration;
+	private Registry _registry;
+	private String hostname;
+	private String clientport;
+	public static void main(String[] args) {
+		try {
+			if (args.length == 1) {
+				VJdbcConfiguration.init(args[0]);
+			} else if (args.length == 2) {
+				Properties props = new Properties();
+				FileInputStream propsInputStream = null;
+				try {
+					propsInputStream = new FileInputStream(args[1]);
+					props.load(propsInputStream);
+					VJdbcConfiguration.init(args[0], props);
+				} finally {
+					if (propsInputStream != null)
+						propsInputStream.close();
+				}
+			} else {
+				throw new RuntimeException("You must specify a configuration file as the first parameter");
+			}
 
-    private RmiConfiguration _rmiConfiguration;
-    private Registry _registry;
+			ConnectionServer connectionServer = new ConnectionServer();
+			connectionServer.serve();
+		} catch (Throwable e) {
+			_logger.error(e.getMessage(), e);
+		}
+	}
 
-    public static void main(String[] args) {
-        try {
-            if(args.length == 1) {
-                VJdbcConfiguration.init(args[0]);
-            } else if(args.length == 2) {
-                // Second argument is a properties file with variables that are
-                // replaced by Digester when the configuration is read in
-                Properties props = new Properties();
-                FileInputStream propsInputStream = null;
-                try {
-                    propsInputStream = new FileInputStream(args[1]);
-                    props.load(propsInputStream);
-                    VJdbcConfiguration.init(args[0], props);
-                } finally {
-                    if(propsInputStream != null) {
-                        propsInputStream.close();
-                    }
-                }
-            } else {
-                throw new RuntimeException("You must specify a configuration file as the first parameter");
-            }
+	public void serve() throws IOException {
+		this.hostname = System.getProperty("java.rmi.server.hostname");
+		this.clientport = System.getProperty("java.rmi.client.port");
+		_logger.info("bind hostname " + hostname);
+		_logger.info("bind client port " + clientport);
+		this._rmiConfiguration = VJdbcConfiguration.singleton().getRmiConfiguration();
 
-            ConnectionServer connectionServer = new ConnectionServer();
-            connectionServer.serve();
-        } catch (Throwable e) {
-            _logger.error(e.getMessage(), e);
-        }
-    }
+		if (this._rmiConfiguration == null) {
+			_logger.debug("No RMI-Configuration specified in VJdbc-Configuration, using default configuration");
+			this._rmiConfiguration = new RmiConfiguration();
+		}
 
-    public ConnectionServer() {
-    }
+		if (this._rmiConfiguration.isUseSSL()) {
+			_logger.info("Using SSL sockets for communication");
+			RMISocketFactory.setSocketFactory(new SecureSocketFactory());
+		}
 
-    public void serve() throws IOException {
-        _rmiConfiguration = VJdbcConfiguration.singleton().getRmiConfiguration();
+		if (this._rmiConfiguration.isCreateRegistry()) {
+			_logger.info("Starting RMI-Registry on port " + this._rmiConfiguration.getPort());
+			this._registry = LocateRegistry.createRegistry(this._rmiConfiguration.getPort(), new RMIClientSocketFactory() {
+				public Socket createSocket(String host, int port) throws IOException {
+					if(port == 0 && clientport!=null){
+						port = Integer.valueOf(clientport);
+					}
+					System.out.println("RMIServerSocketFactory().createSocket()");
+					InetAddress addr = InetAddress.getByName(host);
+					if (addr.equals(InetAddress.getLocalHost())) {
+						return new Socket(addr, port);
+					} else {
+						throw new IOException("remote socket bind forbidden.");
+					}
+				}
+			}, new RMIServerSocketFactory() {
+				public ServerSocket createServerSocket(int port) throws IOException {
+					// String hostname =
+					// System.getProperty("java.rmi.server.hostname");
+					System.out.println("RMIServerSocketFactory().createServerSocket()");
+					return new ServerSocket(port, 0, InetAddress.getByName(hostname));
+				}
+			});
+		} else {
+			_logger.info("Using RMI-Registry on port " + this._rmiConfiguration.getPort());
+			this._registry = LocateRegistry.getRegistry(hostname, this._rmiConfiguration.getPort());
+		}
 
-        if(_rmiConfiguration == null) {
-            _logger.debug("No RMI-Configuration specified in VJdbc-Configuration, using default configuration");
-            _rmiConfiguration = new RmiConfiguration();
-        }
+		installShutdownHook();
 
-        if(_rmiConfiguration.isUseSSL()) {
-            _logger.info("Using SSL sockets for communication");
-            RMISocketFactory.setSocketFactory(new SecureSocketFactory());
-        }
+		_logger.info("Binding remote object to '" + this._rmiConfiguration.getObjectName() + "'");
+		this._registry.rebind(this._rmiConfiguration.getObjectName(), new ConnectionBrokerRmiImpl(this._rmiConfiguration.getRemotingPort()));
+	}
 
-        if(_rmiConfiguration.isCreateRegistry()) {
-            _logger.info("Starting RMI-Registry on port " + _rmiConfiguration.getPort());
-            _registry = LocateRegistry.createRegistry(_rmiConfiguration.getPort());
-        } else {
-            _logger.info("Using RMI-Registry on port " + _rmiConfiguration.getPort());
-            _registry = LocateRegistry.getRegistry(_rmiConfiguration.getPort());
-        }
-
-        installShutdownHook();
-
-        _logger.info("Binding remote object to '" + _rmiConfiguration.getObjectName() + "'");
-        _registry.rebind(_rmiConfiguration.getObjectName(), new ConnectionBrokerRmiImpl(_rmiConfiguration.getRemotingPort()));
-    }
-
-    private void installShutdownHook() {
-        // Install the shutdown hook
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                try {
-                    _logger.info("Unbinding remote object");
-                    _registry.unbind(_rmiConfiguration.getObjectName());
-                } catch (RemoteException e) {
-                    _logger.error("Remote exception", e);
-                } catch (NotBoundException e) {
-                    _logger.error("Not bound exception", e);
-                }
-            }
-        }));
-    }
+	private void installShutdownHook() {
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			public void run() {
+				try {
+					ConnectionServer._logger.info("Unbinding remote object");
+					ConnectionServer.this._registry.unbind(ConnectionServer.this._rmiConfiguration.getObjectName());
+				} catch (RemoteException e) {
+					ConnectionServer._logger.error("Remote exception", e);
+				} catch (NotBoundException e) {
+					ConnectionServer._logger.error("Not bound exception", e);
+				}
+			}
+		}));
+	}
 }
